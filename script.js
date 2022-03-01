3fe26592119a7041cc95ff6ea08b74264646b369
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-01 19:46:06 +0530

    Event Loop - CallStack

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // Event Loop In-depth in Telugu | JavaScript
 console.clear();
 
+/* 
+01. Event Loop 
+02. CallStack
+03. CallBack Queue
+04. MicroTask Queue
+*/
+
+
+console.log('Start');
+
+let myFunction = () => console.log('My Function');
+myFunction();
+
+console.log('End');
